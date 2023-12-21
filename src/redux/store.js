04f344fb889c9bbd387bd1fc3fa0808c3f1ef040
bireswarghosh <116499672+bireswarghosh@users.noreply.github.com>
2023-12-21 20:31:37 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,4 +16,4 @@ const store = configureStore({
 
 export default store;
 // ! add the backend server hosted link
-export const server = "http://localhost:4000/api/v1";
+export const server = "https://burger-app-backend-xw6n.onrender.com/api/v1";
